d9218bfc5c09847b41eadd99b9fd2e2b70168543
jia1000 <jiayifeng@deepwise.com>
2019-03-22 15:03:21 +0800

    Demo版本：添加了Controller的使用方式，如，带不带参数。

--- src/main/java/com/example/demo/controler/HelloworldController.java
+++ src/main/java/com/example/demo/controler/HelloworldController.java
@@ -1,15 +1,31 @@
 package com.example.demo.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloworldController {
 
+    // 第一个页
     @RequestMapping("/hello")
     @ResponseBody
     public String sayhello() {
         return "hello world .    this is first about Spirng Boot.";
+    }
+
+    // 第二个页
+    @RequestMapping("/talk")
+    @ResponseBody
+    public String talk() {
+        return "I'm talking.";
+    }
+
+    // 第三个页，带参数的请求
+    @RequestMapping("/user/{id}")
+    @ResponseBody
+    public String getById(@PathVariable Long id) {
+        return "user id : " + id;
     }
 }
