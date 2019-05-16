7da568c97820a6c18d0a3bb6edabc95ceebe8f57
jiaYF <jiajiajiajia10003@163.com>
2019-05-16 11:44:13 +0800

    Demo版本：添加数据库的使用。之二。增加了SQL的查询、删除等操作。如，按name查询、按id查询，以及分页查询等。

--- src/main/java/com/example/demo/controler/StudentController.java
+++ src/main/java/com/example/demo/controler/StudentController.java
@@ -3,9 +3,14 @@ package com.example.demo.controler;
 import com.example.demo.dao.StudentRepository;
 import com.example.demo.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -36,14 +41,48 @@ public class StudentController {
         return "student name : " + student_name;
     }
 
-    // 2、 查询，根据id
-    // 测试url ：http://127.0.0.1:8080/student/get/2
-    @GetMapping(value = "/get/{id}")
+    // 2-1、 查询，根据id
+    // 测试url ：http://127.0.0.1:8080/student/get_by_id/2
+    @GetMapping(value = "/get_by_id/{id}")
     @ResponseBody
-    public Student queryStudent(@PathVariable(value="id") Integer id) {
+    public Student queryStudentById(@PathVariable(value="id") Integer id) {
         // 后面还有添加get()函数，之前，一直没注意该接口
         Student student = studentRepository.findById(id).get();
         return  student;
+    }
+
+    // 2-2、 查询，根据name
+    // 测试url ：http://127.0.0.1:8080/student/get_by_name/lisi
+    @GetMapping(value = "/get_by_name/{name}")
+    @ResponseBody
+    public Student queryStudentByName(@PathVariable(value="name") String name) {
+        // 后面还有添加get()函数，之前，一直没注意该接口
+        Student student = studentRepository.findByName(name);
+        return  student;
+    }
+
+    // 2-3、 查询全部
+    // 测试url ：http://127.0.0.1:8080/student/get_all
+    @RequestMapping("/get_all")
+    @ResponseBody
+    public List<Student> queryAllStudent() {
+        // 后面还有添加get()函数，之前，一直没注意该接口
+        List<Student> students = studentRepository.findAll();
+        return  students;
+    }
+
+    // 2-4、 查询-使用分页Page
+    // 测试url ：
+    @RequestMapping("/get_by_name_with_page/{name}")
+    @ResponseBody
+    public Page<Student> queryStudentByNameWithPage(@PathVariable(value = "name") String name) throws Exception {
+        // 页码，是从0开始计的
+        int page = 0;
+        int size = 5;
+        Sort sort = new Sort(Sort.Direction.ASC, "id");
+        Pageable pageable = new PageRequest(page, size, sort);
+        Page<Student> stus = studentRepository.findByNameWithPage(name, pageable);
+        return  stus;
     }
 
     // 3、 删除，根据id
@@ -52,7 +91,10 @@ public class StudentController {
     @ResponseBody
     public String deleteStudent(@PathVariable(value="id") Integer id) {
         Student student = studentRepository.findById(id).get();
-        studentRepository.deleteById(id);
+//        studentRepository.deleteById(id);
+        studentRepository.deleteByStudentId(id);
         return "delete student name : " + student.getName();
     }
+
+
 }
--- src/main/java/com/example/demo/dao/StudentRepository.java
+++ src/main/java/com/example/demo/dao/StudentRepository.java
@@ -2,12 +2,35 @@ package com.example.demo.dao;
 
 
 import com.example.demo.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 继承JpaRepository来完成对数据库表的操作
+ *
+ *  @Query           ：查询方法上面使用注解，
+ *  @Modifying      ：涉及到删除和修改，需要加上它.
+ *  @Transactional  ：是对事物的支持
  */
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-//    public Student findByName(String name);
+
+    @Transactional
+    @Query(value = "select * from Student where name = ?1",nativeQuery=true)
+    public Student findByName(String name);
 //    public Student findByid(Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Student where id = ?1")
+    public void deleteByStudentId(Integer id);
+
+    @Transactional
+    @Query(value="select stu from Student stu where stu.name = ?1")
+    public Page<Student> findByNameWithPage(String name, Pageable pageable);
 }
