c16904ff9df1e484d45ed367f0e5996e30df5783
jiaYF <jiajiajiajia10003@163.com>
2019-05-17 17:47:40 +0800

    Demo版本：增加了文件上传功能。 fileupload（但，没有成功。）

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,5 +20,19 @@ public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }    
+    }
+
+    // 简单文件上传 ：1、启动类设置
+    @Bean
+    // 为了解决，上传文件大于10M出现连接重置的问题。此异常内容 GlobalException 也捕获不到。
+    public TomcatServletWebServerFactory tomcatEmbedded() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer)connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxExtensionSize(-1);
+            }
+        });
+        return  tomcat;
+    }
 }
