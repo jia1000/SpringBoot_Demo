d37782dc1a7c5666b976b76f38582fd1f604c638
jiaYF <jiajiajiajia10003@163.com>
2019-07-19 18:58:24 +0800

    Demo版本：修改接口，并解决返回字符串中文乱码的问题。 测试地址： http://localhost:8080/admin/getblog/3

--- src/main/java/com/example/demo/controler/admin/BlogController.java
+++ src/main/java/com/example/demo/controler/admin/BlogController.java
@@ -5,6 +5,7 @@ import com.example.demo.entity.blog.TableTbBlog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,14 +17,14 @@ public class BlogController {
     @Autowired
     private TableTbBlogMapper tableTbBlogMapper;
 
-    @RequestMapping("/blogs")
+    // 解决中文乱码
+    @RequestMapping(value="/getblog/{id}", produces = "application/json; charset=utf-8")
     @ResponseBody
     @Transactional
-    public String getBlogs() {
-        Long id = 5L;
+    public String getBlog(@PathVariable Long id) {
         TableTbBlog blog_item = tableTbBlogMapper.selectByPrimaryKey(id);
         String content = blog_item.getBlog_content();
-        System.out.println("content: " + content);
+        System.out.println("content: ");
         return "blog's content : " + content;
     }
 
