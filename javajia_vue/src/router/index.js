4ffce4628c2ab02ea31b1418669879806cdcccd4
jiaYF <jiajiajiajia10003@163.com>
2019-11-21 11:10:43 +0800

    Demo版本：添加的前端显示vue init代码 。 添加了前端Login相关代码。

--- javajia_vue/src/router/index.js
+++ javajia_vue/src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
 
+//////////////////////////////////////////////////////////////////
+import BlogLogin from '@/components/manage/BlogLogin.vue'
+import BlogIndex from '@/components/home/BlogIndex.vue'
+//////////////////////////////////////////////////////////////////
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [    
+	{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
+    } ,
+    {
+      path: '/index',
+      name: 'BlogIndex',
+      component: BlogIndex
+    } ,
+    {
+      path: '/manage',
+      redirect: '/login'
+    } ,
+    {
+      path: '/login',
+      name: 'BlogLogin',
+      component: BlogLogin
     }
   ]
 })
